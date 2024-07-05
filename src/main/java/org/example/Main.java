66a0d756f9b0ee68cd8c8e441d9e83c3cb62e368
hans <hans@zanker-online.de>
2024-07-05 23:04:34 +0200

    Bugfixes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.example.PDFfile.readPDF;
@@ -9,12 +10,15 @@ import static org.example.Person.createPerson;
 public class Main {
     public static void main(String[] args) {
 
+        List<Person> chor = new ArrayList<>();
+
         //PDF-Liste in einzelne Zeilen zerlegen. Jede Zeile entspricht einer Person (unnötige Zeilen werden gefiltert)
         List<String> PDFlines = readPDF("C:\\Users\\hans.SERVER\\IdeaProjects\\PDFtoOffice\\Data\\Geburtstagsliste.pdf");
 
         //aus jeder Zeile ein Personen-Objekt erzeugen
         for(int i=0; i<PDFlines.size(); i++){
             Person p = createPerson(PDFlines.get(i));
+            chor.add(p);
 
             System.out.println(p.toString());
         }
--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Person {
 
     private String Stimme;
@@ -7,6 +10,8 @@ public class Person {
     private String Name;
     private String Anschrift;
     private String PLZ;
+    private String Ort;
+    private String Mobil;
     private String Geburtsdatum;
     private Boolean Aktiv;
 
@@ -33,8 +38,20 @@ public class Person {
                     p.Stimme = "";
             }
 
+            //Restliche Bestandteile mit Leerzeichen splitten
+            String[] lineParts = PDFline.split(" ");
+
             //wenn eine Person einer Stimme zugeordnet ist, ist sie 'Aktiv".
             p.Aktiv= !p.Stimme.isEmpty();
+            p.Vorname=lineParts[1];
+            p.Name=lineParts[2];
+            p.Anschrift=lineParts[3]+" "+lineParts[4];
+            p.PLZ=lineParts[5];
+            p.Ort=lineParts[6];
+            p.Mobil=lineParts[7]+lineParts[8]+lineParts[9];
+            p.Geburtsdatum=lineParts[10];
+
+
 
         }
         return p;
